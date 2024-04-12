dcafaea376d073b3ff8ec399667c67476f9f9f05
Cilbert Leon <leon@nikitha.in>
2024-04-12 10:32:40 +0530

    part commit

--- src/app/christian-songs/[slug]/[id]/page.js
+++ src/app/christian-songs/[slug]/[id]/page.js
@@ -1,54 +1,67 @@
 import React from "react";
 import Image from "next/image";
-import ThumbCard from "../../../components/ThumbCard";
-
-export default function Home({ params }) {
-    
-    const festivals = ['Good Friday','Easter','Palm Sunday','Christmas'];
+import axios from 'axios';
+import { fetchData } from "@/fe-handlers/requestHandlers";
+import ThumbCard from "@/app/components/ThumbCard";
+import ThumbCardRel from "@/app/components/ThumbCardRel";
+const getData = async (id) => {
+    console.log("slug",id);
+    let data = await fetchData("lang_videos", [
+        {
+          $match: {
+            id: Number(id)
+          }
+        },
+        {
+          $lookup: {
+            from: "lang_videos_related",
+            localField: "id",
+            foreignField: "cat_id",
+            as: "related_data"
+          }
+        }
+      ])
+    return data[0];
+}
+const slugify = (text) => {
+return text.toString().toLowerCase()
+  .replace(/\s+/g, '-')           // Replace spaces with -
+  .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+  .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+  .replace(/^-+/, '')             // Trim - from start of text
+  .replace(/-+$/, '');            // Trim - from end of text
+};
+export default async function Home({ params }) {
+    const data = await getData(params.id);
+    const firstRelatedData = data.related_data[0];
+    // const slug = slugify(data.title);
+   // console.log(data);
     return (
         <div className="">
             <div className="bg-indigo-100 py-6 md:py-12">
                 <div className="container px-4 mx-auto">
-                    <h1 className="text-3xl font-bold text-center mb-5">My Post: {params.id}</h1>
-                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-5">
-                        {
-                            Array(12).fill(0).map((_, index) => (
-                                <ThumbCard key={index} />
-                            ))
-                        
-                        }
+                <div class="flex flex-wrap">
+                    <div class="w-full md:w-2/3 lg:w-8/12 p-4">
+                        <h1 className="text-3xl font-bold mb-5 capitalize">{data.title}</h1>
+                        <div className="ratio ratio-16x9 shadow bg-white rounded-lg overflow-hidden">
+                            <iframe src={`https://www.youtube.com/embed/${firstRelatedData.video}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        <div className="p-3 my-3 shadow bg-white rounded-md">{data.content}</div>
                     </div>
-                    <nav aria-label="Page navigation example" className="flex items-center justify-between">
-                    <span class="text-sm text-gray-700 dark:text-gray-400">
-                        Showing <span class="font-semibold text-gray-900 dark:text-white">1</span> to <span class="font-semibold text-gray-900 dark:text-white">10</span> of <span class="font-semibold text-gray-900 dark:text-white">100</span> Videos
-                    </span>
-                        <ul class="inline-flex -space-x-px text-base h-10">
-                            <li>
-                            <a href="#" class="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
-                            </li>
-                            <li>
-                            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-                            </li>
-                            <li>
-                            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-                            </li>
-                            <li>
-                            <a href="#" aria-current="page" class="flex items-center justify-center px-4 h-10 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-                            </li>
-                            <li>
-                            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
-                            </li>
-                            <li>
-                            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
-                            </li>
-                            <li>
-                            <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
-                            </li>
-                        </ul>
-                    </nav>
+                    <div class="w-full md:w-1/3 lg:w-4/12 p-4">
+                        <h2 className="text-2xl font-bold mb-3">Related Videos</h2>
+                        <div className="grid grid-cols-2 md:grid-cols-1 lg:grid-cols-1 gap-6 mb-5">
+                            {
+                                data.related_data.map((video, index) => (
+                                    <ThumbCardRel key={index} video={video} /> 
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
                 </div>
             </div>
-            
+
         </div>
     );
 }
---
+++ src/app/components/Pagination.js
@@ -0,0 +1,32 @@
+export default function Pagination(props) {
+    return (
+        <nav aria-label="Page navigation example" className="flex items-center justify-between">
+            <span className="text-sm text-gray-700 dark:text-gray-400">
+                Showing <span className="font-semibold text-gray-900 dark:text-white">1</span> to <span className="font-semibold text-gray-900 dark:text-white">10</span> of <span className="font-semibold text-gray-900 dark:text-white">100</span> Videos
+            </span>
+            <ul className="inline-flex -space-x-px text-base h-10">
+                <li>
+                    <a href="#" className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+                </li>
+                <li>
+                    <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+                </li>
+                <li>
+                    <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+                </li>
+                <li>
+                    <a href="#" aria-current="page" className="flex items-center justify-center px-4 h-10 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+                </li>
+                <li>
+                    <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+                </li>
+                <li>
+                    <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
+                </li>
+                <li>
+                    <a href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
